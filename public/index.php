80427e309a1e438cb9c79785697b796123d4329f
Icaro William <icarowilliam@me.com>
2020-05-29 23:50:28 -0300

    installed performance php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "Hi there!";
+/*
+ * Require Performance
+ */
+
+require_once('../vendor/autoload.php');
+
+use Performance\Performance;
+
+/*
+ * One simply performance check
+ */
+
+Performance::point();
+
+// Run task A
+$size = 10000000;
+$users = [];
+
+for ($i = 0; $i < $size; $i++) {
+    $users[$i] = rand();
+}
+
+// Finish all tasks and show test results
+Performance::results();
